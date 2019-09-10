802204b788d5ad149eef69441c8adc800af9f216
Aidan Smith <aidansmith1243@win.cis.ksu.edu>
2019-09-10 14:33:18 -0500

    Finished all of the entree classes without comments

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        private uint nuggets = 6;
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() {};
+                for (int i = 0; i < nuggets; i++)
+                {
+                    ingredients.Add("Chicken Nugget");
+                }
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 59 * nuggets;
+        }
+
+        public void AddNugget()
+        {
+            this.nuggets++;
+            this.Price += 0.25;
+            this.Calories += 59;
+        }
+    }
+}
