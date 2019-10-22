e70341698b889eca1e5809d461f61b67f4c3df10
Aidan Smith <aidansmith1243@gmail.com>
2019-10-22 15:43:04 -0500

    Added UI updates from class to change the order box

--- PointOfSale/MainWindow.xaml.cs
+++ PointOfSale/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DinoDiner.Menu;
 
 namespace PointOfSale
 {
@@ -30,6 +31,39 @@ namespace PointOfSale
         public MainWindow()
         {
             InitializeComponent();
+
+            // Test order stuff
+            Order order = DataContext as Order;
+            if (order != null)
+            {
+                order.Items.Add(new DinoNuggets());
+                order.Items.Add(new Sodasaurus());
+                Triceritots t = new Triceritots();
+                t.Size = DinoDiner.Menu.Size.Large;
+                order.Items.Add(t);
+                SteakosaurusBurger sb = new SteakosaurusBurger();
+                sb.HoldBun();
+                sb.HoldMustard();
+                order.Items.Add(sb);
+            }
+        }
+
+        private void PassOnDataContext()
+        {
+            Page page = OrderUI.Content as Page;
+            if(page != null)
+            {
+                page.DataContext = OrderUI.DataContext;
+            }
+        }
+
+        public void OnLoadCompleted(object sender, NavigationEventArgs args)
+        {
+            PassOnDataContext();
+        }
+        public void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            PassOnDataContext();
         }
     }
 }
--- PointOfSale/SideSelection.xaml.cs
+++ PointOfSale/SideSelection.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DinoDiner.Menu;
 
 namespace PointOfSale
 {
@@ -42,6 +43,39 @@ namespace PointOfSale
             Medium.Background = Brushes.LightGray;
             Small.Background = Brushes.LightGray;
             btn.Background = Brushes.Gray;
+
+            if (DataContext is Order order)
+            {
+                if(CollectionViewSource.GetDefaultView(order.Items).CurrentItem is  Side side)
+                switch (btn.Name)
+                {
+                    case "Small":
+                            side.Size = DinoDiner.Menu.Size.Small;
+                        break;
+                    case "Medium":
+                            side.Size = DinoDiner.Menu.Size.Medium;
+                            break;
+                    case "Large":
+                            side.Size = DinoDiner.Menu.Size.Large;
+                            break;
+                }
+            }
+
+        }
+        /// <summary>
+        /// Adds fryceritops to the order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddFryceritops(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                Side s = new Fryceritops();
+                order.Items.Add(s);
+                CollectionViewSource.GetDefaultView(order.Items).MoveCurrentToLast();
+            }
+
         }
     }
 }
