5d3477d90f859c4a4b32c95ef6dd3ad4d9be0e83
Aidan Smith <aidansmith1243@gmail.com>
2019-11-16 11:47:05 -0600

    finished website milestone 1

--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
 
 namespace Website.Pages
 {
     public class MenuModel : PageModel
     {
+        public Menu menu;
         public void OnGet()
         {
-
+            menu = new Menu();
+            
+        }
+        public string GetIngredients(IMenuItem m)
+        {
+            string str = "Ingredients: ";
+            foreach(string i in m.Ingredients)
+            {
+                str += i + ", ";
+            }
+            return str.Substring(0, str.Length - 2); 
         }
     }
 }
